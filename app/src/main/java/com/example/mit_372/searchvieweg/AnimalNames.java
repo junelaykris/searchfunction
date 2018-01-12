3e61c8cd6689acb9854b2fa514e50c0ed72ba8db
junelaykris <junelay6695@gmail.com>
2018-01-12 14:27:15 +0630

    first commit

---
+++ app/src/main/java/com/example/mit_372/searchvieweg/AnimalNames.java
@@ -0,0 +1,16 @@
+package com.example.mit_372.searchvieweg;
+
+/**
+ * Created by MIT-372 on 4/11/2017.
+ */
+
+public class AnimalNames {
+    private String animalName;
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+}
---
+++ app/src/main/java/com/example/mit_372/searchvieweg/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.mit_372.searchvieweg;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] animalNameList;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Generate sample data
+
+        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey"};
+
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < animalNameList.length; i++) {
+            AnimalNames animalNames = new AnimalNames(animalNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+       // editsearch = (SearchView) findViewById(R.id.simpleSearchView);
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+
+        inflater.inflate(R.menu.menu_layout,menu);
+
+        MenuItem Searchviewitem=menu.findItem(R.id.action_search);
+
+       editsearch=(SearchView) MenuItemCompat.getActionView(Searchviewitem);
+       editsearch.setIconifiedByDefault(false);
+        editsearch.setOnQueryTextListener(this);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
